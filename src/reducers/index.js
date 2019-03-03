96c77ae9fea4532c51d9901f33351a6515ae718d
yashraikar1087 <yashraikar1087@gmail.com>
2019-03-03 12:06:56 -0700

    songs reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducers = () => {
+  return [
+    {
+      title: "Barbie Girl",
+      duration: "4.05"
+    },
+    {
+      title: "Backstreet Boys",
+      duration: "2.05"
+    },
+    {
+      title: "All Star",
+      duration: "3.17"
+    }
+  ];
+};
